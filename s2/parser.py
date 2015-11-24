35f5527872d1b169d991b8b4dd4089b06d0fc5ef
Robert Åberg <robertab@kth.se>
2015-11-24 15:40:46 +0100

    Tog bort hjälpfunktionen till innerParse

--- s2/parser.py
+++ s2/parser.py
@@ -4,9 +4,11 @@
 Author:         Robert Åberg, Sara Ervik
 Assignment:     S2, Sköldpaddegrafik
 File:           parser.py
+
+Här beräkans utdata utifrån trädet som skickas in.
+Trädet är uppbyggt av next och down pekare som branchas
+bara när en rep instruktion kommer, dvs en down.
 """
-
-
 
 from constants import *
 from math import *
@@ -22,33 +24,9 @@ def printOutput():
             print('%.4f' % float(output[i]), end="\t") 
         print()
         
-
 def calculateAngle(tree):
     angle = (pi * tree.num) / 180
     return angle
-
-def innerParse(tree):
-    global DOWN
-    global ANGLE
-    output[1] = output[3]
-    output[2] = output[4]
-    if tree == None: return 0
-    else:
-        if tree.token.type == 'down'    :   DOWN = True
-        elif tree.token.type == 'up'    :   DOWN = False
-        elif tree.token.type == 'color' :   output[0] = tree.num
-        elif tree.token.type == 'left'  :   ANGLE += calculateAngle(tree)
-        elif tree.token.type == 'right' :   ANGLE -= calculateAngle(tree)
-        elif tree.token.type == 'forw'  :
-            output[3] += tree.num * cos(ANGLE)
-            output[4] += tree.num * sin(ANGLE)
-            printOutput()
-        elif tree.token.type == 'back':
-            output[3] -= tree.num * cos(ANGLE)
-            output[4] -= tree.num * sin(ANGLE)
-            printOutput()
-        elif tree.token.type == 'rep': return parse(tree)
-    innerParse(tree.next)
 
 def parse(tree):
     global DOWN
@@ -72,7 +50,7 @@ def parse(tree):
             printOutput()
         elif tree.token.type == 'rep':
             for i in range(tree.num):
-                innerParse(tree.down)
+                parse(tree.down)
     parse(tree.next)
     
 if __name__ == '__main__':
