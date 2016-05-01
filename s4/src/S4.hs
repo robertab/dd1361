902b727944aea2b2589fc2b7ac13bfb8b8d42e90
Robert Åberg <robertab@kth.se>
2016-05-01 19:13:33 +0200

    Fixat samtliga instruktioner från S2.

--- s4/src/S4.hs
+++ s4/src/S4.hs
@@ -7,26 +7,74 @@ module S4
           ) where
 
 
-
-
 import Text.Parsec (ParseError, parse, many1, digit, many, try, spaces, char)
 import Text.Parsec.String (Parser)
 import Text.Parsec.Char (anyChar, string, oneOf, space )
 import Data.Char
-import Control.Applicative ((<|>))
+import Control.Applicative ((<|>), (<$>), (<*), (*>), (<$))
 import Control.Monad (void)
 
 
-data Instruction = Forw Integer | Back Integer deriving Show
+data Instruction
+  = Forw Integer
+  | Back Integer
+  | LeftI Integer
+  | RightI Integer
+  | Color Integer
+  | Down
+  | Up
+  deriving ((Show))
 
 regularParse :: Parser a  -> String -> Either ParseError a
 regularParse p = parse p ""
 
 testInstruction :: Parser Instruction
-testInstruction = try forw <|> back
+testInstruction = try forw <|> back <|> left <|> right <|> up <|> down <|> color
 
 parseTest :: String -> Either ParseError Instruction
 parseTest = regularParse forw 
+
+down :: Parser Instruction
+down = do
+  f <- string "DOWN"
+  spaces
+  char '.'
+  return Down
+
+up :: Parser Instruction
+up = do
+  f <- string "UP"
+  spaces
+  char '.'
+  return Up
+  
+color :: Parser Instruction
+color = do
+  f <- string "COLOR"
+  many1 space
+  char '#'
+  col <- many1 digit
+  spaces
+  char '.'
+  return (Color (read col))
+
+left :: Parser Instruction
+left = do
+  f <- string "LEFT"
+  many1 space
+  n <- many1 digit
+  spaces
+  char '.'
+  return (LeftI (read n))
+
+right :: Parser Instruction
+right = do
+  f <- string "RIGHT"
+  many1 space
+  n <- many1 digit
+  spaces
+  char '.'
+  return (RightI (read n))
 
 forw :: Parser Instruction
 forw = do
